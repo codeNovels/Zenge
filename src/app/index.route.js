61ce18cca28adbd745eb7afe8925a35b1e55bac2
Billy Nguyen <codeNovels@gmail.com>
2017-06-04 22:22:55 -0500

    first build to deploy

--- src/app/index.route.js
+++ src/app/index.route.js
@@ -11,7 +11,7 @@
     {
         $locationProvider.html5Mode(true);
 
-        $urlRouterProvider.otherwise('/user');
+        $urlRouterProvider.otherwise('/games');
 
         /**
          * Layout Style Switcher
